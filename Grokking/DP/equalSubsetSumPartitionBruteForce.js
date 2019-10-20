3be7c4dfcf32fae1b70019f9e96910227a7a66cb
Janavi Anand <janavianand@yahoo.com>
2019-10-20 14:59:20 -0400

    equal subset sum partition brute force

---
+++ Grokking/DP/equalSubsetSumPartitionBruteForce.js
@@ -0,0 +1,19 @@
+const can_partition = function(num) {
+  // TODO: Write your code here
+  let sum = num.reduce((a,b)=>a+b,0)
+  if(num.length === 0 || sum%2) return false
+  return helper(num,0,sum/2);
+};
+
+function helper(num,currentIndex,sum){
+  if(currentIndex >= num.length) return false
+
+  if(num[currentIndex] === sum) return true
+
+  return helper(num,currentIndex+1,sum) || helper(num,currentIndex+1,sum-num[currentIndex])
+
+}
+
+console.log(`Can partition: ${can_partition([1, 2, 3, 4])}`)
+console.log(`Can partition: ${can_partition([1, 1, 3, 4, 7])}`)
+console.log(`Can partition: ${can_partition([2, 3, 4, 6])}`)
