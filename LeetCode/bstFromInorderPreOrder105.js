31f4896f2daf91aa080c06dc19d9fa8cd7ab6b9c
Janavi Anand <janavianand@yahoo.com>
2019-12-11 08:55:07 -0500

    const bst from inorder and preorder

---
+++ LeetCode/bstFromInorderPreOrder105.js
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {number[]} preorder
+ * @param {number[]} inorder
+ * @return {TreeNode}
+ */
+var buildTree = function(preorder, inorder) {
+    return create(preorder,inorder,0,inorder.length-1,0,preorder.length-1)
+};
+
+function create(pre,ino,instart,inend,prestart,preend){
+    if(prestart > preend || instart > inend){
+        return null
+    }
+
+    let root_val = pre[prestart]
+
+    let root = new TreeNode(root_val)
+
+    let index = ino.indexOf(root_val)
+
+    let leftLimit = index - instart
+    let rightLimit = inend - index
+
+    root.left = create(pre,ino,instart,index-1,prestart+1,prestart+leftLimit)
+    root.right = create(pre,ino,index+1,inend,prestart+leftLimit+1,prestart+leftLimit+rightLimit)
+
+    return root
+
+}
