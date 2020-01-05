f77a8cfef0117bfbc919175217e845f43391201a
Janavi Anand <janavianand@yahoo.com>
2020-01-05 17:10:12 -0500

    252. Meeting Rooms

---
+++ bloombergLeetCode/easy/252Meeting Rooms.js
@@ -0,0 +1,36 @@
+/*
+
+Given an array of meeting time intervals consisting of start and end times [[s1,e1],[s2,e2],...] (si < ei), determine if a person could attend all meetings.
+
+Example 1:
+
+Input: [[0,30],[5,10],[15,20]]
+Output: false
+Example 2:
+
+Input: [[7,10],[2,4]]
+Output: true
+
+Link - https://leetcode.com/problems/meeting-rooms/
+*/
+
+/**
+ * @param {number[][]} intervals
+ * @return {boolean}
+ */
+var canAttendMeetings = function(intervals) {
+
+    if(intervals.length < 2){
+        return true
+    }
+
+    intervals.sort((a,b)=>a[0]-b[0])
+
+    for(let i=0;i<intervals.length-1;i++){
+        if(intervals[i][1] > intervals[i+1][0]){
+            return false
+        }
+    }
+
+    return true
+};
